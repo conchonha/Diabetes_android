7882dbf37823251ea8bc0a1362ca7bd5b06e8aed
sangthaiba <kzumi2110>
2021-01-11 19:21:41 +0700

    update project 100

--- app/src/main/java/com/example/diabetes/activity/BloodPressureChartActivity.java
+++ app/src/main/java/com/example/diabetes/activity/BloodPressureChartActivity.java
@@ -1,16 +1,26 @@
 package com.example.diabetes.activity;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.Dialog;
 import android.graphics.Color;
+import android.media.Image;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.PopupMenu;
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import com.example.diabetes.R;
 import com.example.diabetes.model.HealthInformationModel;
 import com.example.diabetes.services.APIServices;
 import com.example.diabetes.services.DataService;
 import com.example.diabetes.util.Helpers;
+import com.example.diabetes.util.dialog.DialogShow;
 import com.github.mikephil.charting.charts.BarChart;
 import com.github.mikephil.charting.data.BarData;
 import com.github.mikephil.charting.data.BarDataSet;
@@ -24,62 +34,138 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
-public class BloodPressureChartActivity extends AppCompatActivity {
-    private ArrayList<BarEntry>arrayList =  new ArrayList<>();
+public class BloodPressureChartActivity extends AppCompatActivity implements View.OnClickListener {
+    private ArrayList<BarEntry> arrayList = new ArrayList<>();
+    private Dialog mDialog;
+    private ImageView mImgMenu;
+    private List<HealthInformationModel> mList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_blood_pressure_chart);
+        initView();
+        init();
+        listenerOnclicked();
+    }
+
+    private void listenerOnclicked() {
+        findViewById(R.id.img_back).setOnClickListener(this);
+        findViewById(R.id.img_menu).setOnClickListener(this);
+    }
+
+    private void showChart(String str){
+        BarChart barChart = findViewById(R.id.barChart);
+
+        BarDataSet barDataSet = new BarDataSet(arrayList, str);
+        barDataSet.setColors(ColorTemplate.MATERIAL_COLORS);
+        barDataSet.setValueTextColor(Color.BLACK);
+        barDataSet.setValueTextSize(16f);
+
+        BarData barData = new BarData(barDataSet);
+
+        barChart.setFitBars(true);
+        barChart.setData(barData);
+        barChart.getDescription().setText(str+" Chart");
+        barChart.animateY(2000);
+    }
+
+
+    private void init() {
+        mDialog = DialogShow.showdialog(BloodPressureChartActivity.this);
+        mDialog.show();
 
         DataService dataService = APIServices.getService();
-        Call<List<HealthInformationModel>>call = dataService.getBloodPressureChart();
+        Call<List<HealthInformationModel>> call = dataService.getBloodPressureChart();
 
         call.enqueue(new Callback<List<HealthInformationModel>>() {
             @Override
             public void onResponse(Call<List<HealthInformationModel>> call, Response<List<HealthInformationModel>> response) {
-                if(response.isSuccessful()){
-                    if(response.body().size() > 0){
-                        for (int i = 0; i < response.body().size() ; i++){
-                            arrayList.add(new BarEntry(i+1,response.body().get(i).getBloodPressure()));
-                        }
-                        BarChart barChart = findViewById(R.id.barChart);
-
-                        BarDataSet barDataSet = new BarDataSet(arrayList,"blood pressure");
-                        barDataSet.setColors(ColorTemplate.MATERIAL_COLORS);
-                        barDataSet.setValueTextColor(Color.BLACK);
-                        barDataSet.setValueTextSize(16f);
-
-                        BarData barData = new BarData(barDataSet);
-
-                        barChart.setFitBars(true);
-                        barChart.setData(barData);
-                        barChart.getDescription().setText("Blood Pressure Chart");
-                        barChart.animateY(2000);
-                    }else{
-                        Helpers.showAlertDialog("Không tìm thấy kết quả nào",BloodPressureChartActivity.this);
-                        close();
+                mDialog.dismiss();
+                mList = response.body();
+                if (response.isSuccessful()) {
+                    for (int i = 0; i < mList.size(); i++) {
+                        arrayList.add(new BarEntry(i + 1, mList.get(i).getWeights()));
                     }
-                }else{
-                    Toast.makeText(BloodPressureChartActivity.this, response.message(), Toast.LENGTH_SHORT).show();
-                    close();
+                    showChart("Weights");
+                } else {
+                    Helpers.showAlertDialog(response.message(), BloodPressureChartActivity.this);
                 }
             }
 
             @Override
             public void onFailure(Call<List<HealthInformationModel>> call, Throwable t) {
-                Toast.makeText(BloodPressureChartActivity.this, t.getMessage(), Toast.LENGTH_SHORT).show();
-                close();
+                mDialog.dismiss();
+                Helpers.showAlertDialog(t.getMessage(), BloodPressureChartActivity.this);
             }
         });
-
     }
-    private void close(){
-        try {
-            Thread.sleep(3000);
-            finish();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+
+    private void initView() {
+        mImgMenu = findViewById(R.id.img_menu);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.img_back:
+                finish();
+                break;
+            case R.id.img_menu:
+                PopupMenu popupMenu = new PopupMenu(this,mImgMenu);
+                popupMenu.inflate(R.menu.menu_chart);
+                popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem item) {
+                        switch (item.getItemId()){
+                            case R.id.weights:
+                                arrayList.clear();
+                                for (int i = 0; i < mList.size(); i++) {
+                                    arrayList.add(new BarEntry(i + 1, mList.get(i).getWeights()));
+                                }
+                                showChart("Weights");
+                                break;
+                            case R.id.height:
+                                arrayList.clear();
+                                for (int i = 0; i < mList.size(); i++) {
+                                    arrayList.add(new BarEntry(i + 1, mList.get(i).getHeights()));
+                                }
+                                showChart("Height");
+                                break;
+                            case R.id.boolPressure:
+                                arrayList.clear();
+                                for (int i = 0; i < mList.size(); i++) {
+                                    arrayList.add(new BarEntry(i + 1, mList.get(i).getBloodPressure()));
+                                }
+                                showChart("Bool Pressure");
+                                break;
+                            case R.id.bloodSugar:
+                                arrayList.clear();
+                                for (int i = 0; i < mList.size(); i++) {
+                                    arrayList.add(new BarEntry(i + 1, mList.get(i).getBloodSugar()));
+                                }
+                                showChart("Blood Sugar");
+                                break;
+                            case R.id.cpr:
+                                arrayList.clear();
+                                for (int i = 0; i < mList.size(); i++) {
+                                    arrayList.add(new BarEntry(i + 1, mList.get(i).getCPR()));
+                                }
+                                showChart("CPR");
+                                break;
+                            case R.id.hdl:
+                                arrayList.clear();
+                                for (int i = 0; i < mList.size(); i++) {
+                                    arrayList.add(new BarEntry(i + 1, mList.get(i).getHDLC()));
+                                }
+                                showChart("HDL-C");
+                                break;
+                        }
+                        return true;
+                    }
+                });
+                popupMenu.show();
+                break;
         }
     }
 }
--- app/src/main/java/com/example/diabetes/services/APIServices.java
+++ app/src/main/java/com/example/diabetes/services/APIServices.java
@@ -2,10 +2,10 @@ package com.example.diabetes.services;
 
 public class APIServices {
     private static String ip = "http://192.168.1.37";
-    private static String baseurl="http://192.168.1.6/Diabetes_Laravel/public/";
+    private static String baseurl="http://192.168.1.9/Diabetes_Laravel/public/";
    // private static String baseurl = ip + "/diabetes_laravel/public/";
     //public static String urlImage = ip + "/diabetes_laravel/public/";
-    public static String urlImage="http://192.168.1.6/Diabetes_Laravel/public/";
+    public static String urlImage="http://192.168.1.9/Diabetes_Laravel/public/";
 
     public static DataService getService() {
         return APIRetrofitClient.getClient(baseurl).create(DataService.class);
